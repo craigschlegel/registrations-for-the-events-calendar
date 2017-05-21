72b1d3820fdd54ccd39f73a9a28c2dece1291c3b
Craig Schlegel <craigmschlegel@gmail.com>
2017-05-21 17:21:31 -0500

    removed hr, tracked image foler

--- inc/admin/templates/form.php
+++ inc/admin/templates/form.php
@@ -21,5 +21,4 @@ settings_errors(); ?>
     <hr />
     <?php do_settings_sections( 'rtec_advanced' ); ?>
     <input class="button-primary" type="submit" name="save" value="<?php esc_attr_e( 'Save Changes' ); ?>" />
-    <hr />
 </form>
